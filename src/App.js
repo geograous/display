7a0cc998efd0278f64546b6ccbca725070365df5
zhuhao <geograous@126.com>
2020-10-18 01:20:16 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Table } from "antd";
+import logo from "./logo.svg";
+import "antd/dist/antd.css";
+import "./App.css";
+
+import list from "./data.json";
+
+console.log("data:", list);
+
+function Item({ cdn, title }) {
+  return (
+    <div className="Item">
+      <img src={cdn} />
+      <h5>{title}</h5>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <section>
+        <Table dataSource={list} pagination={false}>
+          <Table.Column
+            width="160px"
+            title="结果"
+            render={(item) => <Item {...item} />}
+          />
+          <Table.Column
+            title="对比"
+            render={(row) => (
+              <div>
+                {row.items.map((item, idx) => (
+                  <Item key={idx} {...item} />
+                ))}
+              </div>
+            )}
+          />
+        </Table>
+      </section>
     </div>
   );
 }
